06fe25bb9e2f2b60636c14bfe17b05a338494fbf
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2021-11-25 18:15:15 +0700

    Read File TXT

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public void readFileText(String filePath) {
+        try {
+            // Đọc file theo đường dẫn
+            File file = new File(filePath);
+
+            // Kiểm tra nếu file không tồn tại thì ném ra ngoại lệ.
+            if (!file.exists()) {
+                    throw new FileNotFoundException();
+            }
+
+            // Đọc từng dòng của file và tiến hành cộng tổng lại
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+
+            // Hiển thị ra màn hình tổng các số nguyên trong file
+            System.out.println("Tổng = " + sum);
+        } catch (Exception e) {
+            // TH file không tồn tại hoặc nội dung file có lỗi thì sẽ hiển thị thông báo lỗi.
+            System.out.println("?");
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        String path = "E:\\CODEGYM\\C0921K1\\untitled3\\src\\com\\company\\file\\number.txt";
+        Main main = new Main();
+       main.readFileText(path);
+    }
+}
